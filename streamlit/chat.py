c710f1330d52eba8f49b056ef729bd2072288fc6
CGLee <chulgil.lee@riman.com>
2025-04-01 15:42:37 +0900

    Feat: add a few shot feature

--- streamlit/chat.py
+++ streamlit/chat.py
@@ -6,8 +6,7 @@ from langchain_community.document_loaders import Docx2txtLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from dotenv import load_dotenv
 
-from llm import get_ai_message
-
+from llm import get_ai_response
 
 st.set_page_config(page_title="소득세 챗봇", page_icon=":shark:")
 st.title("소득세 챗봇")
@@ -21,9 +20,7 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
         
-
 print(f"before: {st.session_state.messages}")
-
 
 if user_question := st.chat_input(placeholder="질문을 입력하세요"):
     with st.chat_message("user"):
@@ -31,9 +28,9 @@ if user_question := st.chat_input(placeholder="질문을 입력하세요"):
     st.session_state.messages.append({"role": "user", "content": user_question})    
     
     with st.spinner("답변을 생성하는 중입니다."):
-        ai_message = get_ai_message(user_question)
+        ai_response = get_ai_response(user_question)
         with st.chat_message("assistant"):
-            st.write(ai_message)
+            ai_message = st.write_stream(ai_response)
         st.session_state.messages.append({"role": "assistant", "content": ai_message})
 
 
